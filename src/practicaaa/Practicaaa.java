be764fd512d629bc7ef26ed324bee8feae502a22
noeminas2007-cyber <noeminas2007@gmail.com>
2026-04-28 16:53:53 -0600

    Conexion

---
+++ src/practicaaa/Practicaaa.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practicaaa;
+
+/**
+ *
+ * @author umg
+ */
+public class Practicaaa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
